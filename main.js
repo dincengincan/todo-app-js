7e1a0e87dc475b9864c425e1c5cbdb149a185aff
dincengincan <engincandinc@gmail.com>
2020-10-08 16:41:59 +0300

    Add complete todo and delete todo logic

--- main.js
+++ main.js
@@ -2,21 +2,27 @@ const input = document.getElementById("input")
 const todoList = document.getElementById("list")
 const button = document.getElementById("add-button")
 
-const li = document.createElement("li")
-const cancelButton= document.createElement("button")
-const checkButton= document.createElement("button")
-
 const addToDo = (value) => {
+    const li = document.createElement("li")
+    const cancelButton= document.createElement("button")
+    const checkButton= document.createElement("button")
+    const label= document.createElement("label")
 
     todoList.appendChild(li);
-    li.innerHTML = value;
 
+    li.appendChild(label);
     li.appendChild(checkButton);
     li.appendChild(cancelButton);
+
+    label.innerHTML = value;
+    
     cancelButton.innerHTML= "X";
-    checkButton.innerHTML= "Edit";
+    checkButton.innerHTML= "Done";
     cancelButton.setAttribute("id", "cancel-button");
     checkButton.setAttribute("id", "check-button");
+
+    checkButton.addEventListener("click", onCheck);
+    cancelButton.addEventListener("click", onCancel);
 }
 
 const onClick = () => {
@@ -25,14 +31,17 @@ const onClick = () => {
 }
 
 const onCheck = (e) => {
-    console.log(e.target.innerHTML)
+    if(e.target.innerHTML === "Done"){ 
+        e.target.parentNode.firstChild.className = "label-line-through";
+        e.target.innerHTML = "Undone"
+    }else{
+        e.target.parentNode.firstChild.className = "label-normal"
+        e.target.innerHTML = "Done"
+    }
 }
 
 const onCancel = (e) => {
-    console.log(e.target.innerHTML,"cancel")
+    e.target.parentNode.remove()
 }
 
-
-todoList.addEventListener("click", onCheck);
-cancelButton.addEventListener("click", onCancel);
 button.addEventListener("click", onClick);
