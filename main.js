d525a4ad7604163d8c6147a6aaa66582c8c76862
dincengincan <engincandinc@gmail.com>
2020-10-10 05:10:58 +0300

    Add style

--- main.js
+++ main.js
@@ -7,17 +7,20 @@ const addToDo = (value) => {
     const cancelButton= document.createElement("button")
     const checkButton= document.createElement("button")
     const label= document.createElement("label")
+    const div= document.createElement("div")
 
     todoList.appendChild(li);
 
     li.appendChild(label);
-    li.appendChild(checkButton);
-    li.appendChild(cancelButton);
+    li.appendChild(div);
+    div.appendChild(checkButton);
+    div.appendChild(cancelButton);
 
     label.innerHTML = value;
     
     cancelButton.innerHTML= "X";
     checkButton.innerHTML= "Done";
+    div.setAttribute("id", "button-container")
     cancelButton.setAttribute("id", "cancel-button");
     checkButton.setAttribute("id", "check-button");
 
@@ -32,16 +35,16 @@ const onClick = () => {
 
 const onCheck = (e) => {
     if(e.target.innerHTML === "Done"){ 
-        e.target.parentNode.firstChild.className = "label-line-through";
+        e.target.parentNode.parentNode.firstChild.className = "label-line-through";
         e.target.innerHTML = "Undone"
     }else{
-        e.target.parentNode.firstChild.className = "label-normal"
+        e.target.parentNode.parentNode.firstChild.className = "label-normal"
         e.target.innerHTML = "Done"
     }
 }
 
 const onCancel = (e) => {
-    e.target.parentNode.remove()
+    e.target.parentNode.parentNode.remove()
 }
 
 button.addEventListener("click", onClick);
